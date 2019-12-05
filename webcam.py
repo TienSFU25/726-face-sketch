ec0173fc8e57d0f39132bc7949b563f567d432a9
Tien Vu <tienv2510@gmail.com>
2019-12-04 21:48:11 -0800

    Improve webcam

--- webcam.py
+++ webcam.py
@@ -1,18 +1,30 @@
 import cv2
+import re
+from os import listdir
+from os.path import isfile, join
+
+save_to_folder = "./frames"
+reg = re.compile('cam(\d).jpg')
+indices = [int(reg.match(f).group(1)) for f in listdir(save_to_folder)]
+i = max(indices)
 
 cap = cv2.VideoCapture(0)
-
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    processed_frame = cv2.putText(frame, "Press C to capture", (100, 100),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+
+    cv2.imshow('frame', processed_frame)
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
         break
+    if key == ord('c'):
+        cv2.imwrite(save_to_folder + "/cam" + str(i) + ".jpg", frame)
+        i += 1
 
 # When everything done, release the capture
 cap.release()
